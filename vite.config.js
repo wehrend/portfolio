6ccfc058efe48b7016c2b7b5f7765b4808c07944
Sven Wehrend <swehrend@ant-neuro.com>
2026-05-27 15:59:43 +0200

    added footer and impressum

--- vite.config.js
+++ vite.config.js
@@ -1,6 +1,16 @@
 import { defineConfig } from "vite";
 import tailwindcss from "@tailwindcss/vite";
+import { resolve } from "path";
 
 export default defineConfig({
   plugins: [tailwindcss()],
+  build: {
+    rollupOptions: {
+      input: {
+        // Hier definierst du die Einstiegspunkte für deine Seiten
+        main: resolve(__dirname, "index.html"),
+        impressum: resolve(__dirname, "impressum.html"),
+      },
+    },
+  },
 });
